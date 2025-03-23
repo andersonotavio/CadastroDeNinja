6f76ceb723ea7bc370086de0f08f1fa4880569f8
Anderson Otavio dos Santos <andersonotavio@hotmail.com>
2025-03-23 15:32:04 -0300

    fist commit

---
+++ src/main/java/com/otavioweb/cadastroDeNinja/CadastroDeNinjaApplication.java
@@ -0,0 +1,13 @@
+package com.otavioweb.cadastroDeNinja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjaApplication.class, args);
+	}
+
+}
